092de80c3da2a5b8596eb9c03e210be445f80719
Leonardo Sanches <51983317+LeonardoGabrielSanches@users.noreply.github.com>
2019-10-02 20:39:58 -0300

    - que merda

--- ApiApplication/Startup.cs
+++ ApiApplication/Startup.cs
@@ -17,7 +17,7 @@ namespace VendasAPI
         {
             Configuration = configuration;
         }
-
+        //aa
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
