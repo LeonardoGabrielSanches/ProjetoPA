4c06eb49295262aad9f64e2a8c5edaa0d104659a
Leonardo Sanches <51983317+LeonardoGabrielSanches@users.noreply.github.com>
2019-10-02 21:58:34 -0300

    sem is required

--- InfraSql/Mapping/ClienteMapping.cs
+++ InfraSql/Mapping/ClienteMapping.cs
@@ -11,16 +11,16 @@ namespace VendasAPI.Infra.Mapping
             builder.ToTable("Cliente");
             builder.HasKey(b => b.ClienteID).HasName("ClienteID");
             builder.Property(b => b.ClienteID).HasColumnName("ClienteID").ValueGeneratedOnAdd();
-            builder.Property(b => b.Bairro).HasColumnName("Bairro").HasMaxLength(4000).IsRequired();
-            builder.Property(b => b.CEP).HasColumnName("CEP").HasMaxLength(9).IsRequired();
-            builder.Property(b => b.email).HasColumnName("Email").HasMaxLength(4000).IsRequired();
-            builder.Property(b => b.Logradouro).HasColumnName("Logradouro").HasMaxLength(4000).IsRequired();
-            builder.Property(b => b.Municipio).HasColumnName("Município").HasMaxLength(4000).IsRequired();
-            builder.Property(b => b.Nome).HasColumnName("NomeCliente").HasMaxLength(4000).IsRequired();
-            builder.Property(b => b.Numero).HasColumnName("Número").HasMaxLength(100).IsRequired();
-            builder.Property(b => b.NumeroDocumento).HasColumnName("Número do documento").HasMaxLength(20).IsRequired();
-            builder.Property(b => b.TipoDocumento).HasColumnName("TipoDoDocumento").HasMaxLength(50).IsRequired();
-            builder.Property(b => b.UF).HasColumnName("UF").HasMaxLength(5).IsRequired();
+            builder.Property(b => b.Bairro).HasColumnName("Bairro").HasMaxLength(4000);
+            builder.Property(b => b.CEP).HasColumnName("CEP").HasMaxLength(9);
+            builder.Property(b => b.email).HasColumnName("Email").HasMaxLength(4000);
+            builder.Property(b => b.Logradouro).HasColumnName("Logradouro").HasMaxLength(4000);
+            builder.Property(b => b.Municipio).HasColumnName("Município").HasMaxLength(4000);
+            builder.Property(b => b.Nome).HasColumnName("NomeCliente").HasMaxLength(4000);
+            builder.Property(b => b.Numero).HasColumnName("Número").HasMaxLength(100);
+            builder.Property(b => b.NumeroDocumento).HasColumnName("Número do documento").HasMaxLength(20);
+            builder.Property(b => b.TipoDocumento).HasColumnName("TipoDoDocumento").HasMaxLength(50);
+            builder.Property(b => b.UF).HasColumnName("UF").HasMaxLength(5);
         }
     }
 }
